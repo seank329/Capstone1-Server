8f23ae3e3a336e0d7b3d34e30b623730a15685a8
seank329 <seankamran@gmail.com>
2020-02-17 17:47:06 -0500

    bug fixes3

--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ module.exports = {
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     JWT_SECRET:process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
-    CLIENT_ORIGIN:'http://localhost://3000'
+    CLIENT_ORIGIN:'http://localhost:3000'
 };
