c588aedb49dc8d74f9095a14156cae3db185c7bf
seank329 <seankamran@gmail.com>
2020-02-17 14:12:47 -0500

    commit before deploying database

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://sean:123@localhost/memory_general',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://sean:123@localhost/memory_general',
+    TEST_DB_URL: process.env.TEST_DATABASE_URL,
     JWT_SECRET:process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '20s'
 };
