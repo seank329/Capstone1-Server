015c1c3b96daaa3ef803b59261b3a6c92e3aa175
seank329 <seankamran@gmail.com>
2020-02-18 09:05:08 -0500

    Handling cors issues22

--- src/app.js
+++ src/app.js
@@ -28,12 +28,12 @@ app.options('*', cors())
 
 
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-//     next();
-//   });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    next();
+  });
 
 app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
