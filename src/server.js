bc0ebdf053e58b8700b481aca2c6539a95239c79
seank329 <seankamran@gmail.com>
2020-02-17 21:08:59 -0500

    Handling cors issues13

--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ const db = knex({
     connection:DATABASE_URL,
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://memory-app-sigma.now.sh"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.set('db', db)
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost: ${PORT} `)
