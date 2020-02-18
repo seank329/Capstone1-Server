870129cb94e6dfa9982b25b45afcd6101c022613
seank329 <seankamran@gmail.com>
2020-02-17 20:06:00 -0500

    Handling cors issues4

--- src/app.js
+++ src/app.js
@@ -42,9 +42,9 @@ let corsOptions = {
   }
 
 
-app.options('*', cors())
+//app.options('*', cors())
 app.use('/api/users', cors(corsOptions), usersRouter);
-app.use('/api/auth', cors(corsOptions), authRouter);
+app.use('/api/auth', authRouter);
 app.use('/api/memory-general', cors(corsOptions), generalRouter);
 
 app.get('/', (req, res) => {
