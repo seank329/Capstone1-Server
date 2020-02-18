db6e7e45eb1b62406d906f04bab1ab1cb24a4d0f
seank329 <seankamran@gmail.com>
2020-02-18 09:18:06 -0500

    Handling cors issues23

--- src/app.js
+++ src/app.js
@@ -24,16 +24,14 @@ app.use(morgan(morganOption, { skip: () => NODE_ENV === 'test' }));
 app.use(cors(corsOptions));
 app.use(helmet());
 
-app.options('*', cors())
+// app.options('*', cors())
 
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+//     next();
+//   });
 
 app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
