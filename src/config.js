72d59ab0a50570fe5b459e940eed486527e7c24d
seank329 <seankamran@gmail.com>
2020-02-24 22:49:59 -0500

    refactored back-end, changed back-end api to reflect production

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'production',
-    DATABASE_URL: process.env.DATABASE_URL,//'postgres://uddnwkwxnbawuj:d2e6ea0b00b6c885209fca79026018bca00cba84eddabcdc4cdc98d2e530c620@ec2-184-72-235-159.compute-1.amazonaws.com:5432/d8l3nho10lf5h5',
+    DATABASE_URL: 'postgres://uddnwkwxnbawuj:d2e6ea0b00b6c885209fca79026018bca00cba84eddabcdc4cdc98d2e530c620@ec2-184-72-235-159.compute-1.amazonaws.com:5432/d8l3nho10lf5h5',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     JWT_SECRET:process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '600s',
-    CLIENT_ORIGIN: `http://localhost:3000` //process.env.CLIENT_ORIGIN
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
 };
