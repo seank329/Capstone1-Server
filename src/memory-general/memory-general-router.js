2c2d7c0728dbe47c0cf85b892d7219cb2a5cc1f7
seank329 <seankamran@gmail.com>
2020-02-25 16:02:44 -0500

    Resolved CORS

--- src/memory-general/memory-general-router.js
+++ src/memory-general/memory-general-router.js
@@ -26,7 +26,6 @@ generalRouter
 // Route for getting, posting, and updating player data
 generalRouter
     .route('/player/:id')
-    .use(requireAuth)
     .get(async (req,res,next) => {
         await MemoryGeneralService.getPlayerStats(req.app.get('db'), req.params.id)
         .then(data => {
