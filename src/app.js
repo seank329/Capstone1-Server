8dea24da855a54bc47af82a71a8365634d9214fc
seank329 <seankamran@gmail.com>
2020-02-25 12:38:33 -0500

    cors issues after refactoring

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common' ;
 app.use(morgan(morganOption, { skip: () => NODE_ENV === 'test' }));
 app.use(cors({
     origin: CLIENT_ORIGIN,
-    allowedHeaders : 'Content-Type, Authorization'
+    allowedHeaders : 'content-type, authorization, application/json, OPTIONS'
 }));
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", 'https://memory-app-sigma.now.sh');
